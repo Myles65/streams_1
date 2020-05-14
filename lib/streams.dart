2e5de1a3995c124300cc6b3d908bcecc6700e3f0
Myles Wilson <myles65@gmail.com>
2020-05-13 21:22:08 -0400

    Initial commit

---
+++ lib/streams.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Streams extends StatefulWidget {
+  @override
+  _StreamsState createState() => _StreamsState();
+}
+
+class _StreamsState extends State<Streams> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.check),
+            title: Text('Cutting Hair'),
+            subtitle:Text('Developed'),
+            trailing: Icon(Icons.menu),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.access_time),
+            title: Text('Lawn Company'),
+            subtitle:Text('In Development'),
+            trailing: Icon(Icons.menu),
+            onTap: (){},
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
+            backgroundColor: Color(0xFF006994),
+          ),
+        ],
+      )
+    );
+  }
+}
